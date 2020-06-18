1d0025836e1b185492a05f6669a21b687c3ef8bf
lixianguangguang <664159787@qq.com>
2020-06-18 13:33:16 +0800

    使用idea提交文件到github

---
+++ src/test/java/com/git/ideatest/IdeatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.ideatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeatestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
